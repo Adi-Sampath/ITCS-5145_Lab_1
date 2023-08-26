a682a5c72d23d8177a777fb46e9a64b43488c7dc
Aditya100401 <adityasampath01@gmail.com>
2023-08-26 18:16:43 -0400

    Commit

--- main.c
+++ main.c
@@ -48,11 +48,12 @@ int main (int argc, char* argv[]) {
     for(int i = 0; i < argc; i++) {
         printf("%s\n", argv[i]);
     }
-    int function_id = argv[0];
-    int a = argv[1];
-    int b = argv[2];
-    int n = argv[3];
-    int intensity = argv[4];
+    int function_id = (int) argv[0];
+    int a = (int) argv[1];
+    int b = (int) argv[2];
+    int n = (int) argv[3];
+    int intensity = (int) argv[4];
+     
     clock_t t; // t represents clock ticks which is of type 'clock_t'
     t = clock(); // start clock
     double r = calc_numerical_integration(function_id, a, b, n, intensity);
